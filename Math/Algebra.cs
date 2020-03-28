d7f55c16f72dd2ce7780f83d9f2330a7018254de
1oopr0 <1oo.ooooo.pro@gmail.com>
2020-03-28 13:13:32 +0900

    MathLib

---
+++ Math/Algebra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class Algebra
+    {
+        public static (double RootPlus, double RootMinus) GetRootsOfQuadraticEquation(double a, double b, double c)
+        {
+            var pm = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+
+            return ((-b + pm) / (2 * a), (-b - pm) / (2 * a));
+        }
+    }
+}
---
+++ Math/Geometry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class Geometry
+    {
+        public static (double RotateX, double RotateY) GetRotatePointBy(double x, double y, double centerX, double centerY, double angle)
+        {
+            var cos = Math.Cos(angle);
+            var sin = Math.Sin(angle);
+
+            //rX - cX = cos(x-cX) - sin(y-cY)
+            //rY - cY = sin(x-cX) + cos(y-cY)
+            var rX = cos * (x - centerX) - sin * (y - centerY) + centerX;
+            var rY = sin * (x - centerX) + cos * (y - centerY) + centerY;
+
+            return (rX, rY);
+        }
+    }
+}
